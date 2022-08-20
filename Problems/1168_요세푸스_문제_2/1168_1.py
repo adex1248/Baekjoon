2b680c155a7ff13465c307da2cf6aea115941ccb
adex1248 <40044759+adex1248@users.noreply.github.com>
2022-08-20 18:56:19 +0900

    Create 1168_1.py
    시간 초과 (2%)

---
+++ Problems/1168_요세푸스_문제_2/1168_1.py
@@ -0,0 +1,19 @@
+#2022.08.19
+#1168 요세푸스 문제 2
+#import sys
+#sys.stdin.readline()
+n, k = map(int, input().split())
+
+print("<", end = "")
+
+data = [1 for i in range(n)]
+ind = 0
+for i in range(n-1):
+    ind += k - 1
+    while ind >= n - i: #len(data)
+        ind = ind - (n - i)
+    
+    print(data.pop(ind), end = ", ")
+    
+    
+print(data.pop(), end = ">\n")
